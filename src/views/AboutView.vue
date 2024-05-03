1b848430af88d25202dbac9ea7442e94d8fd12ab
Hugo Yudi Hiraishi <hugoyudy3@gmail.com>
2024-05-03 00:48:51 -0300

    adição do about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>Unimar</h2>
+    <h3>Análise e Desenvolvimento de Sistemas - 3° termo</h3>
+    <h3>Hugo Yudy Hiraishi - 1961997</h3>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
